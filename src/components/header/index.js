15f659387290cbe5fc431b5cf48b035e291655f8
VRegison <vitorregison01@gmail.com>
2023-02-16 17:12:07 -0300

    .

---
+++ src/components/header/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './header.css';
+
+const Header = () => {
+  return (
+    <section className="sectionHeader">
+      <div className="blackBackground">
+        <div className="inbox">
+          <h1>Welcome to my portfolio!</h1>
+          <p>Here you will find a selection of my projects and programming skills. I am always looking for new challenges and learning opportunities in the technology field. Please feel free to explore my work and contact me if you have any questions or are interested in working together. Hope you enjoy my portfolio and thanks for visiting!</p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Header;
